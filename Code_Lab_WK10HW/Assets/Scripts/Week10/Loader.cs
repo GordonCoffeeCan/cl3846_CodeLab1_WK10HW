c1bcb1496bfcca772cf61e0bc263832665c38d5a
Unknown <gordon.lee.6170@gmail.com>
2017-04-05 15:37:24 -0400

    bla bla bla

---
+++ Code_Lab_WK10HW/Assets/Scripts/Week10/Loader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        AsteriodData loadAD = new AsteriodData("Asteroid.json");
+
+        print(loadAD.position);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
